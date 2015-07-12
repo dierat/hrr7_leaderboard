3f99d05d9a5c643a4f7447bec6fdeb5e16b9cad0
dierat <diedra.rater@gmail.com>
2015-07-12 10:19:25 -0700

    sets scores to 0 initially and changes the players to the members of the hrr7 cohort

--- leaderboard/collections.js
+++ leaderboard/collections.js
@@ -4,12 +4,12 @@ Players = new Mongo.Collection("players");
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+      var names = ["Liz Penny", "Isabella Tea",
+                   "Nick Ferraro", "Chris Schepman", "Adam Van Antwerp", "John Games", "Alexis Rodriguez", "Andrey Azov", "Charles  Crame", "Chris Griffis", "Cliff Saporta Cheng", "David Craven", "Diedra Rater", "Derek Olson", "Eric King", "Greg Varias", "Hailey Foster", "Jonathan Mason", "Kamerynn Harrah", "Kevin Chiang", "Kwang Kim", "Logan Low", "Luke Savage", "Marq Short", "Nathaniel Parrish", "Nick Balestra", "Owen Dismuke", "Peter Richmond", "Wesley Tsai", "Zach Sebag", "Syed Jafri"];
       _.each(names, function (name) {
         Players.insert({
           name: name,
-          score: Math.floor(Random.fraction() * 10) * 5
+          score: 0
         });
       });
     }
