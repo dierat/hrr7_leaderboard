a195998c8bd1455812d9f3735ddd6d0b2c03b72a
dierat <diedra.rater@gmail.com>
2015-07-12 11:04:32 -0700

    removes inc button and instead reroutes click event to player

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -2,28 +2,12 @@ if (Meteor.isClient) {
   Template.leaderboard.helpers({
     players: function () {
       return Players.find({}, { sort: { score: -1, name: 1 } });
-    },
-    selectedName: function () {
-      var player = Players.findOne(Session.get("selectedPlayer"));
-      return player && player.name;
     }
   });
 
   Template.leaderboard.events({
-    'click .inc': function () {
-      Players.update(Session.get("selectedPlayer"), {$inc: {score: 5}});
-    }
-  });
-
-  Template.player.helpers({
-    selected: function () {
-      return Session.equals("selectedPlayer", this._id) ? "selected" : '';
-    }
-  });
-
-  Template.player.events({
-    'click': function () {
-      Session.set("selectedPlayer", this._id);
+    'click .player': function () {
+      Players.update({_id: this._id}, {$inc: {score: 5}});
     }
   });
 }
